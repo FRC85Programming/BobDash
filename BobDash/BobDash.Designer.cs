40a46e75a23017f1a3517cf4dee66f8f2d38d4c7
Matt Michielsen <matt@michielsen.us>
2017-04-26 00:15:29 -0400

    Tons of good work

--- BobDash/BobDash.Designer.cs
+++ BobDash/BobDash.Designer.cs
@@ -17,6 +17,13 @@
             {
                 components.Dispose();
             }
+
+            if (_hotKeyManager != null)
+            {
+                _hotKeyManager.Dispose();
+                _hotKeyManager = null;
+            }
+
             base.Dispose(disposing);
         }
 
@@ -31,34 +38,39 @@
             this.lblAutoModeLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.lblAutoModeValue = new System.Windows.Forms.Label();
+            this.cameraPictureBox = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblAutoModeLabel
             // 
             this.lblAutoModeLabel.AutoSize = true;
-            this.lblAutoModeLabel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblAutoModeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAutoModeLabel.Location = new System.Drawing.Point(3, 0);
             this.lblAutoModeLabel.Name = "lblAutoModeLabel";
-            this.lblAutoModeLabel.Size = new System.Drawing.Size(220, 109);
+            this.lblAutoModeLabel.Size = new System.Drawing.Size(119, 78);
             this.lblAutoModeLabel.TabIndex = 0;
             this.lblAutoModeLabel.Text = "Auto mode:";
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lblAutoModeValue, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.lblAutoModeLabel, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.lblAutoModeValue, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(453, 219);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(148, 611);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // lblAutoModeValue
@@ -66,17 +78,46 @@
             this.lblAutoModeValue.AutoSize = true;
             this.lblAutoModeValue.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblAutoModeValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAutoModeValue.Location = new System.Drawing.Point(229, 0);
+            this.lblAutoModeValue.Location = new System.Drawing.Point(3, 78);
             this.lblAutoModeValue.Name = "lblAutoModeValue";
-            this.lblAutoModeValue.Size = new System.Drawing.Size(221, 109);
+            this.lblAutoModeValue.Size = new System.Drawing.Size(142, 533);
             this.lblAutoModeValue.TabIndex = 1;
+            this.lblAutoModeValue.Text = "X";
+            // 
+            // cameraPictureBox
+            // 
+            this.cameraPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cameraPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.cameraPictureBox.Name = "cameraPictureBox";
+            this.cameraPictureBox.Size = new System.Drawing.Size(1198, 611);
+            this.cameraPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cameraPictureBox.TabIndex = 2;
+            this.cameraPictureBox.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.cameraPictureBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1350, 611);
+            this.splitContainer1.SplitterDistance = 1198;
+            this.splitContainer1.TabIndex = 4;
             // 
             // BobDash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(453, 219);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(1350, 611);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "BobDash";
             this.Text = "BobDash";
             this.TopMost = true;
@@ -84,6 +125,11 @@
             this.Load += new System.EventHandler(this.BobDash_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraPictureBox)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -93,6 +139,8 @@
         private System.Windows.Forms.Label lblAutoModeLabel;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Label lblAutoModeValue;
+        private System.Windows.Forms.PictureBox cameraPictureBox;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
--- BobDash/BobDash.cs
+++ BobDash/BobDash.cs
@@ -1,31 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using GlobalHotKey;
+using MjpegProcessor;
 using NetworkTables;
 using NetworkTables.Tables;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace BobDash
 {
     public partial class BobDash : Form
     {
+        private const string CAMERA_URI = "http://roboRIO-85-FRC.local:1183/?action=stream";
+
         private System.Timers.Timer _timer;
+        private HotKeyManager _hotKeyManager = new HotKeyManager();
+        private MjpegDecoder _cameraDecoder;
+        private bool _cameraStarted;
+        private double? _autoMode = null;
 
         public BobDash()
         {
             InitializeComponent();
+            DoubleBuffered = true;
         }
 
         private ITable SmartDashboard
         {
             get
             {                
-                var tables = NetworkTable.GetTable("/").GetSubTables();
-                return NetworkTable.GetTable("/");//.GetSubTable("SmartDashboard");
+                return NetworkTable.GetTable("SmartDashboard");
             }
         }
 
@@ -33,7 +37,10 @@ namespace BobDash
         {
             if (lblAutoModeValue.InvokeRequired)
             {
-                lblAutoModeValue.Invoke(new Action(() => { lblAutoModeValue.Text = autoMode; }));
+                if (lblAutoModeValue.Text != autoMode)
+                {
+                    lblAutoModeValue.Invoke(new Action(() => { UpdateAutoModeText(autoMode); }));
+                }
             }
             else
             {
@@ -41,24 +48,152 @@ namespace BobDash
             }
         }
 
+        private void SetBackColor(Color color)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => BackColor = color));
+            }
+            else
+            {
+                BackColor = color;
+            }
+        }
+
         private void BobDash_Load(object sender, EventArgs e)
         {
+            _hotKeyManager.Register(Key.D0, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+            _hotKeyManager.Register(Key.D1, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+            _hotKeyManager.Register(Key.D2, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+            _hotKeyManager.Register(Key.D3, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+            _hotKeyManager.Register(Key.D4, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+            _hotKeyManager.Register(Key.D5, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+            _hotKeyManager.Register(Key.D6, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+            _hotKeyManager.Register(Key.D7, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+            _hotKeyManager.Register(Key.D8, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+            _hotKeyManager.Register(Key.D9, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+            _hotKeyManager.KeyPressed += _hotKeyManager_KeyPressed;
+
+            NetworkTable.AddGlobalConnectionListener((remote, info, connected) =>
+            {
+                OnConnectionChanged(connected);
+            }, true);
+
             NetworkTable.SetClientMode();
-            NetworkTable.SetTeam(85);
+            NetworkTable.SetIPAddress("roborio-85-frc.local");
             NetworkTable.Initialize();
 
-            _timer = new System.Timers.Timer(500);
+            _timer = new System.Timers.Timer(200);
             _timer.Elapsed += _timer_Elapsed;
             _timer.Start();
+            SetupCamera();
+        }
+
+        private void OnConnectionChanged(bool connected)
+        {
+            if (connected)
+            {
+                SetBackColor(Color.LimeGreen);
+                StartCamera();
+            }
+            else
+            {
+                SetBackColor(Color.Red);
+                StopCamera();
+            }
+        }
+
+        private void _hotKeyManager_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            try
+            {
+                _autoMode = Convert.ToDouble(e.HotKey.Key.ToString().TrimStart('D'));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void SetupCamera()
+        {
+            if (_cameraDecoder == null)
+            {
+                _cameraDecoder = new MjpegDecoder();
+                _cameraDecoder.FrameReady += Decoder_FrameReady;
+                _cameraDecoder.Error += _cameraDecoder_Error;
+            }
+        }
+
+        private void StartCamera()
+        {
+            if (_cameraStarted)
+            {
+                return;
+            }
+
+            if (_cameraDecoder == null)
+            {
+                SetupCamera();
+            }
+
+            _cameraDecoder.ParseStream(new Uri(CAMERA_URI));
+
+            _cameraStarted = true;
+        }
+
+        private void StopCamera()
+        {
+            if (!_cameraStarted)
+            {
+                return;
+            }
+
+            if (_cameraDecoder != null)
+            {
+                _cameraDecoder.StopStream();
+            }
+
+            _cameraStarted = false;
+        }
+
+        private void _cameraDecoder_Error(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        private void Decoder_FrameReady(object sender, FrameReadyEventArgs e)
+        {
+            cameraPictureBox.Image = e.Bitmap;
         }
 
         private void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
+            double currentValue = 0;
             try
             {
-                var keys = SmartDashboard.GetKeys();
-                SmartDashboard.PutNumber("AUTO MODE", 10);
-                UpdateAutoModeText(SmartDashboard.GetNumber("AUTO MODE").ToString());
+                currentValue = SmartDashboard.GetNumber("AUTO MODE");
+            }
+            catch
+            {
+                SetBackColor(Color.Yellow);
+                if (_autoMode.HasValue)
+                {
+                    currentValue = _autoMode.Value;
+                }
+            }
+
+            try
+            { 
+                if (_autoMode.HasValue)
+                {
+                    if (_autoMode.Value != currentValue)
+                    {
+                        SmartDashboard.PutNumber("AUTO MODE", _autoMode.Value);
+                    }
+                }
+
+                UpdateAutoModeText(currentValue.ToString());
             }
             catch (Exception ex)
             {
@@ -68,7 +203,7 @@ namespace BobDash
 
         private void BobDash_FormClosing(object sender, FormClosingEventArgs e)
         {
-            NetworkTable.Shutdown();
+            _timer.Stop();
         }
     }
 }
